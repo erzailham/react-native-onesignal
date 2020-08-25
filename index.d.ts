40965fe6325d7da5d216854a6a30a1cb33ec3ba4
Reza Ilham <erzailham@gmail.com>
2020-08-25 11:49:37 +0700

    change Settings interface to optional

--- index.d.ts
+++ index.d.ts
@@ -18,11 +18,24 @@ declare module 'react-native-onesignal' {
 
   type oneSignalEvents = 'received' | 'opened' | 'ids';
 
-  interface Settings {
-    kOSSettingsKeyAutoPrompt: boolean,
-    kOSSettingsKeyInAppLaunchURL: boolean,
-    kOSSSettingsKeyPromptBeforeOpeningPushURL: boolean,
-    kOSSettingsKeyInFocusDisplayOption: InFocusDisplayOption
+  export interface Settings {
+    /** Determines if the application automatically prompts the user for permission to send push notification the first time the application opens. */
+    kOSSettingsKeyAutoPrompt?: boolean
+
+    /** Determines if the application opens push notification URL's in an in-app web browser view, or exits the app and opens the URL in Safari. */
+    kOSSettingsKeyInAppLaunchURL?: boolean
+
+    /** Determines if the app will pop up an alert view to ask the user if they want to open a URL. If this setting is `false`, the application will immediately open the web browser when the user taps a notification with a launch URL. */
+    kOSSSettingsKeyPromptBeforeOpeningPushURL?: boolean
+
+    /**
+     * Determines how the app will display push notifications.
+     *
+     * - `0` = None: When your app is open, OneSignal will not display a notification (it will be up to your app to provide a custom UI to display it)
+     * - `1` = In App Alert: Notifications will display as a popup
+     * - `2` = Notification: Notifications will display as normal notifications.
+     */
+    kOSSettingsKeyInFocusDisplayOption?: 0 | 1 | 2
   }
 
   interface PushData {
